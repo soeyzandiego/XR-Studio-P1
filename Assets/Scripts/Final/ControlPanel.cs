b32affe7b19c52be555a97144e7b881843bfca71
Zoey San Diego <59988129+soeyzandiego@users.noreply.github.com>
2025-03-05 10:36:46 -0500

    Crank!...kind of

--- Assets/Scripts/Final/ControlPanel.cs
+++ Assets/Scripts/Final/ControlPanel.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 
 public class ControlPanel : MonoBehaviour
 {
+    [SerializeField] float panelPosDist = 1f;
+    float minYPos = 3f;
     [SerializeField] GameObject graphics;
     [SerializeField] PropSet[] propSets; // will not be GameObjects
     [SerializeField] Image propIcon;
@@ -19,6 +21,8 @@ public class ControlPanel : MonoBehaviour
     void Start()
     {
         if (instance == null) { instance = this; }
+        propIcon.sprite = propSets[propIndex].icon;
+        minYPos = transform.position.y;
     }
 
     // Update is called once per frame
@@ -35,8 +39,13 @@ public class ControlPanel : MonoBehaviour
 
     void Reposition(Transform player)
     {
+        Vector3 eulers = transform.eulerAngles;
         transform.LookAt(-player.GetComponent<OVRCameraRig>().centerEyeAnchor.forward);
-        transform.position = player.position + player.GetComponent<OVRCameraRig>().centerEyeAnchor.forward * 2.5f;
+        transform.eulerAngles = new Vector3(eulers.x, transform.eulerAngles.y + 180, transform.eulerAngles.z);
+        Vector3 forwardOffset = player.GetComponent<OVRCameraRig>().centerEyeAnchor.forward * panelPosDist;
+        Vector3 newPos = player.position + forwardOffset;
+        newPos.y = Mathf.Max(minYPos, newPos.y);
+        transform.position = newPos;
     }
 
     public void SpawnPropBox()
---
+++ Assets/Scripts/Final/Crank.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Crank : MonoBehaviour
+{
+    float crankValue;
+    float lastCrankValue;
+    float nCrankValue;
+
+    [SerializeField] Light light;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //nCrankValue = Mathf.Abs(crankValue / 45);
+        //light.intensity = nCrankValue;
+        //Debug.Log(crankValue + ", " + nCrankValue);
+
+        crankValue = transform.eulerAngles.x;
+        //crankValue = Mathf.Atan2(transform.right.y, transform.right.x);
+        //crankValue += Mathf.PI;
+        //crankValue /= 2f * Mathf.PI;
+
+        float diff = crankValue - lastCrankValue;
+        light.intensity += diff / 100;
+
+        lastCrankValue = crankValue;
+        Debug.Log(crankValue);
+    }
+}
